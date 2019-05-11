f7a7e6fc572619715abe9d0f13ab675abb6b85b0
terrykay <tel@tezk.co.uk>
2019-05-11 21:23:52 +0100

    Updates

--- src/java/controller/BackgroundMaintenanceBean.java
+++ src/java/controller/BackgroundMaintenanceBean.java
@@ -6,12 +6,16 @@
 package controller;
 
 import entity.Customer;
+import entity.Image;
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Stateless;
 import session.CustomerFacade;
+import session.ImageFacade;
 
 /**
  *
@@ -19,13 +23,30 @@ import session.CustomerFacade;
  */
 @Stateless
 public class BackgroundMaintenanceBean {
+
     @EJB
     CustomerFacade customerFacade;
-    
-    @Schedule(dayOfWeek = "*", month = "*", hour = "3", dayOfMonth = "*", year = "*", minute = "0", second = "0", persistent = false)
+    @EJB
+    ImageFacade imageFacade;
+
+    @Schedule(dayOfWeek = "*", month = "*", hour = "*", dayOfMonth = "*", year = "*", minute = "02", second = "0", persistent = false)
     public void myTimer() {
-        List <Customer>customers = customerFacade.findAll();
-        System.out.println(new Date()+" : "+customers.size()+" customers listed");
+        List<Customer> customers = customerFacade.findAll();
+        System.out.println(new Date() + " : " + customers.size() + " customers listed");
+
+        Runnable task = new Runnable() {
+            public void run() {
+                ImageHandlerBean handler = new ImageHandlerBean();
+                Collection<Image> images = imageFacade.findByType('i');
+                Iterator<Image> i = images.iterator();
+                while (i.hasNext()) {
+                    Image image = i.next();
+                    handler.removeImage(image.getUrl(), image.getCustomerId().getId().toString());
+                }
+                System.out.println("Removed "+images.size()+" images removed");
+            }
+        };
+        new Thread(task).start();
     }
 
 }
